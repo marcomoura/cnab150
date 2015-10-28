1008b36eac5f7ca729f9a382e0a1ef6061843739
Marco Moura <email@marcomoura.com>
2015-10-28 09:29:41 -0200

    Refactoring layout case

--- lib/cnab150/layout.rb
+++ lib/cnab150/layout.rb
@@ -4,16 +4,9 @@ require 'cnab150/errors/layout_not_implemented_error'
 module Cnab150
   module Layout
     def self.build(type)
-      case
-      when type.eql?('A')
-        Cnab150::Layout::A
-      when type.eql?('Z')
-        Cnab150::Layout::Z
-      when type.eql?('G')
-        Cnab150::Layout::G
-      else
-        fail Cnab150::Errors::LayoutNotImplementedError
-      end.new
+      eval("Cnab150::Layout::#{type.upcase}").new
+    rescue NameError
+      fail Cnab150::Errors::LayoutNotImplementedError
     end
   end
 end
--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Cnab150::Layout do
   describe '#build' do
+    context 'when the layout exists' do
+      it { expect( described_class.build('A') ).to be_a_kind_of Cnab150::Layout::A }
+      it { expect( described_class.build('G') ).to be_a_kind_of Cnab150::Layout::G }
+      it { expect( described_class.build('Z') ).to be_a_kind_of Cnab150::Layout::Z }
+    end
+
     context 'when the layout does not exists' do
       it do
         expect { described_class.build('Y') }.to raise_error Cnab150::Errors::LayoutNotImplementedError
